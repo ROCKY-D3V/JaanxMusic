af38b927134c3e49eeaa2ca269c0c22fe69ab0cd
ʀσƈƙყ <146221888+ROCKY-D3V@users.noreply.github.com>
2023-12-10 13:29:44 +0530

    Add files via upload

--- JaanxMusic/utils/inline/play.py
+++ JaanxMusic/utils/inline/play.py
@@ -1,7 +1,7 @@
 import math
-
+from config import SUPPORT_CHAT, OWNER_USERNAME
 from pyrogram.types import InlineKeyboardButton
-
+from JaanxMusic import app
 from JaanxMusic.utils.formatters import time_to_seconds
 
 
@@ -33,26 +33,32 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "🥀—•—•—•—•—•—•—•—•—"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—🥀—•—•—•—•—•—•—•—"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "—•—🥀—•—•—•—•—•—•—"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "—•—•—🥀—•—•—•—•—•—"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "—•—•—•—🥀—•—•—•—•—"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—•—•—•—•—🥀—•—•—•—"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "—•—•—•—•—•—🥀—•—•—"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "—•—•—•—•—•—•—🥀—•—"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "—•—•—•—•—•—•—•—🥀—"
     else:
-        bar = "—————————◉"
+        bar = "—•—•—•—•—•—•—•—•—🥀"
     buttons = [
+                [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
@@ -60,11 +66,21 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
+                 [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
+
+                text="✨ ᴅєᴠ ✨",
+
+                url=f"t.me/{OWNER_USERNAME}",
+
+            ),
+            InlineKeyboardButton(
+
+                text="❤️ ɢʀσυᴘ ❤️",
+
+                url=f"{SUPPORT_CHAT}",
+
+            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -80,6 +96,22 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+                 [
+            InlineKeyboardButton(
+
+                text="✨ ᴅєᴠ ✨",
+
+                url=f"t.me/{OWNER_USERNAME}",
+
+            ),
+            InlineKeyboardButton(
+
+                text="❤️ ɢʀσυᴘ ❤️",
+
+                url=f"{SUPPORT_CHAT}",
+
+            ),
+        ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -90,11 +122,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AviaxPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"JaanxPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AviaxPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"JaanxPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
